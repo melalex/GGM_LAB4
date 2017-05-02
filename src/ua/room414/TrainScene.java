15f042232e8cca7b32b822c817b480641d41cdbf
melalex <melalex490@gmail.com>
2017-05-02 23:30:36 +0300

    Base app

---
+++ src/ua/room414/TrainScene.java
@@ -0,0 +1,78 @@
+package ua.room414;
+
+import javax.media.j3d.*;
+import javax.swing.*;
+import javax.vecmath.Color3f;
+import javax.vecmath.Vector3f;
+
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+import java.awt.*;
+
+
+public class TrainScene extends JFrame {
+    private static final int WINDOW_WIDTH = 640;
+    private static final int WINDOW_HEIGHT = 480;
+    private static final String WINDOW_TITLE = "Lab4";
+
+    private Canvas3D canvas;
+
+    private SimpleUniverse universe;
+
+    private BranchGroup root;
+
+    private void configureWindow() {
+        setTitle(WINDOW_TITLE);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int locationX = (screenSize.width - getWidth()) / 2;
+        int locationY = (screenSize.height - getHeight()) / 2;
+        setLocation(locationX,locationY);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void configureCanvas() {
+        canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+        canvas.setDoubleBufferEnable(true);
+        getContentPane().add(canvas, BorderLayout.CENTER);
+    }
+
+    private void configureUniverse() {
+        universe = new SimpleUniverse(canvas);
+        universe.getViewingPlatform().setNominalViewingTransform();
+    }
+
+    private void addModelToUniverse() {
+        root = new BranchGroup();
+
+        root.addChild(new ColorCube(0.3));
+    }
+
+    private void addLightsToUniverse() {
+        Bounds influenceRegion = new BoundingSphere();
+        Color3f lightColor = new Color3f(Color.BLUE);
+        Vector3f lightDirection = new Vector3f(-1F, -1F, -1F);
+        DirectionalLight light = new DirectionalLight(lightColor, lightDirection);
+        light.setInfluencingBounds(influenceRegion);
+        root.addChild(light);
+    }
+
+    private void render() {
+        configureWindow();
+        configureCanvas();
+        configureUniverse();
+        addModelToUniverse();
+        addLightsToUniverse();
+
+        universe.addBranchGraph(root);
+    }
+
+    public static void main(String[] args) {
+        TrainScene trainScene = new TrainScene();
+
+        trainScene.render();
+
+        trainScene.setVisible(true);
+    }
+}
